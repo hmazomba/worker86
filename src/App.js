d2807c769d6debd9759feec3d791673e3ba04439
HMazomba <hlumzo@gmail.com>
2020-11-22 19:37:48 +0200

    Loading.....

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React, {Fragment, useState, useEffect} from 'react';
 import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState({ hits: []});
-  const[query, setQuery] = useState('google');
-  const[search, setSearch] = useState('');
-  const [url, setUrl] = useState(
+  const [ data, setData] = useState({ hits: []});
+  const [ query, setQuery] = useState('google');
+  const [ search, setSearch] = useState('');
+  const [ url, setUrl] = useState(
     'https:hn.algolia.com/api/v1/search?query=facebook'
-  )
+  );
+  const [ isLoading, setIsLoading ] = useState(false);
 
   useEffect(() => {
     const fetchData = async() => {
+      setIsLoading(true);
       const result = await axios(url);
       setData(result.data);
+      setIsLoading(false);
     }
     fetchData();
   }, [url]);
@@ -26,14 +29,18 @@ function App() {
       <button type="button" onClick={() => setUrl(`https:hn.algolia.com/api/v1/search?query=${query}`)}>
         Search
       </button>
-      <ul>
-        {data.hits.map(item => (
-          <li key={item.objectID}>
-            <a href={item.url}>{item.title}</a>
-          </li>
-        ))}
 
-      </ul>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : ( 
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
     </Fragment>
   );
 }
